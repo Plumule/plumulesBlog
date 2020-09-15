3438328c8ce8ba5f997847559d94dfa2e616d2b5
plumule <705397651@qq.com>
2020-09-16 00:43:47 +0800

    【【【2020-09-16 00:16:28.273 ERROR 13376 --- [io-8080-exec-10] org.thymeleaf.TemplateEngine             : [THYMELEAF][http-nio-8080-exec-10] Exception processing template "index": An error happened during template parsing (template: "class path resource [templates/index.html]") org.thymeleaf.exceptions.TemplateInputException: An error happened during template parsing (template: "class path resource [templates/index.html]") Caused by: org.attoparser.ParseException: Error resolving template "$", template might not exist or might not be accessible by any of the configured Template Resolvers (template: "_fragments" - line 7, col 12)】】】 一字一句找到眼瞎，最后发现 th:replace="${title}" 写成了小括号。。。。。。。。。。我恨我自己(T_T)

--- blog-server/src/main/java/com/lj/web/IndexController.java
+++ blog-server/src/main/java/com/lj/web/IndexController.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name){
+    @GetMapping("/")
+    public String index(){
 //        int i = 9/0;
 //        String blog = null;
 //        if(blog == null){
 //            throw new NotFoundException("博客不存在");
 //        }
-        System.out.println("-------------index-------------");
         return "index";
     }
 }
