e24fd2d3770838bb9bea16ece4ebd44db104e0a4
JamieLee <JamieLee>
2021-01-21 18:18:48 +0800

    关关难过关关过*:ஐ٩(๑´ᵕ`)۶ஐ:* 学习使我进步

--- blog-server/src/main/java/com/lj/web/admin/TypeController.java
+++ blog-server/src/main/java/com/lj/web/admin/TypeController.java
@@ -1,5 +1,6 @@
 package com.lj.web.admin;
 
+import com.lj.po.Type;
 import com.lj.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,7 +20,7 @@ public class TypeController {
     private TypeService typeService;
 
     @GetMapping("/types")
-    public String types(@PageableDefault(size = 10,sort = {"id"},direction = Sort.Direction.DESC)
+    public String types(@PageableDefault(size = 3,sort = {"id"},direction = Sort.Direction.DESC)
                                     Pageable pageable, Model model){
         model.addAttribute("page",typeService.listType(pageable));
         return "admin/types";
@@ -27,5 +29,16 @@ public class TypeController {
     @GetMapping("/types/input")
     public String input(){
         return "admin/types-input";
+    }
+
+    @PostMapping("/types")
+    public String post(Type type){
+        Type t = typeService.saveType(type);
+        if(t == null){
+
+        }else{
+
+        }
+        return "redirect:/admin/types";
     }
 }
